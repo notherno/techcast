b62ba665adab4faf17acd3892bfbf8c80203a166
alitaso <alitaso@pixiv.com>
2018-06-01 09:40:58 +0900

    ランダム再生してそれっぽく見えるように、Rebuildとyatteiki.fmを追加

--- constants.js
+++ constants.js
@@ -1,12 +1,15 @@
 exports.audioData = [
   {
-    title: 'Episode 139',
-    url:
-      'https://feeds.soundcloud.com/stream/274166909-amazon-web-services-306355661-aws-podcast-episode-139.mp3'
+    title: 'Rebuild.fm',
+    url: 'http://cache.rebuild.fm/podcast-ep209.mp3'
   },
   {
-    title: 'Episode 140',
+    title: 'Alexa Dev Chat',
     url:
       'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
+  },
+  {
+    title: 'yatteiki.fm',
+    url: 'https://yatteiki.fm/audio/47.mp'
   }
 ];
